aa0392bb96a9f17232165cf9df08f44ee94099be
seyeonsim <seyeonsimw@gmail.com>
2024-12-24 15:53:09 +0900

    Initial commit

---
+++ src/main/java/com/sesac/guseok_be/GuseokBeApplication.java
@@ -0,0 +1,13 @@
+package com.sesac.guseok_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuseokBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuseokBeApplication.class, args);
+	}
+
+}
