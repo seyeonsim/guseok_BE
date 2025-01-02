2ab8f19934eb2e13fdcaec40aec05d5a0d8aae95
seyeonsim <seyeonsimw@gmail.com>
2025-01-02 14:39:27 +0900

    feat: park - limit 있는 findByDistrict 생성

--- src/main/java/com/sesac/guseok_be/service/CulturalEventService.java
+++ src/main/java/com/sesac/guseok_be/service/CulturalEventService.java
@@ -13,9 +13,15 @@ public class CulturalEventService {
     @Autowired
     CulturalEventRepository culturalEventRepository;
 
-    public List<CulturalEventEntity> getEventList(String district) {
+    public List<CulturalEventEntity> getEventList(String district, Integer limit) {
 //        return culturalEventRepository.findByEndDateAfterAndDistrict(LocalDate.now(), district).stream().limit(10).toList();
-        return culturalEventRepository.findByEndDateAfterAndDistrict(LocalDate.now(), district);
+        List<CulturalEventEntity> events = culturalEventRepository.findByEndDateAfterAndDistrict(LocalDate.now(), district);
 
+        if (limit != null) {
+            // limit 파라미터가 존재하면 해당 개수만큼 제한
+            return events.stream().limit(limit).toList();
+        }
+
+        return events;
     }
 }
